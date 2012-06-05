4ec4449fccf4a827af60897e235f8f3f5f4d247a
Christian Daniel <cd@maintech.de>
2012-06-05 15:23:43 +0200

    DSPEngine: speed optimisation

--- dsp/dspengine.cpp
+++ dsp/dspengine.cpp
@@ -177,7 +177,7 @@ void DSPEngine::work()
 
 		// extract power spectrum and reorder buckets
 		for(int i = 0; i < m_fftSize; i++) {
-			Complex c = m_fftOut[((i + m_fftOut.size() / 2) % m_fftOut.size())];
+			Complex c = m_fftOut[((i + (m_fftOut.size() >> 1)) & (m_fftOut.size() - 1))];
 			Real v = sqrt(c.real() * c.real() + c.imag() * c.imag());
 			v /= (Real)m_fftSize;
 			v = 20.0 * log10(v);
